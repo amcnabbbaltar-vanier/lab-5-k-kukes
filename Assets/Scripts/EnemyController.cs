3466dd24d8a5ff5feb4ea965bded5b3a1eee3579
k-kukes <158488017+k-kukes@users.noreply.github.com>
2026-02-26 15:26:58 -0500

    lab 5 completed

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void GotHit()
+    {
+        animator.SetTrigger("GotHit");
+
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AddScore(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerHitboxManagerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitboxManagerScript : MonoBehaviour
+{
+    public Collider[] attackColliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Collider attackCollider in attackColliders)
+        {
+            attackCollider.enabled = false;
+        }
+    }
+
+    public void EnableHitbox()
+    {
+        foreach(Collider attackCollider in attackColliders)
+        {
+            attackCollider.enabled = true;
+        }
+    }
+
+    public void DisableHitbox()
+    {
+        foreach(Collider attackCollider in attackColliders)
+        {
+            attackCollider.enabled = false;
+        }
+    }
+}
